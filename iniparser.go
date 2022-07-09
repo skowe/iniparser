a91db99d1e04d046e608d1b2cfb9f14afa9b8413
Veselin Petrovic <veselin.petrovic.97@gmail.com>
2022-07-10 00:33:46 +0200

    Added some tests, fixed CRLF handling in windows.

--- iniparser.go
+++ iniparser.go
@@ -11,61 +11,60 @@ import (
 
 //Constants for file parsing
 const (
-	LineEnd = '\n'
-	CommentStart = ';'
-	KeyValSep = "="	
+	WindowsLineEnd = "\r\n"
+	LineEnd        = '\n'
+	CommentStart   = ';'
+	KeyValSep      = "="
 )
 
-// INI type contains a slice of blocks 
+var isWindows = false
+
+// INI type contains a slice of blocks
 // Blocks are held inside the Blocks []Block variable
 // INI type also contains the Raw data
 // RawTrimmed contains all the data with comments trimmed
 // inside the Raw []byte variable
 type INI struct {
-	Blocks []Block
-	Raw []byte
+	Blocks     map[string]Block
+	Raw        []byte
 	RawTrimmed []byte
 }
 
-
 // The Block type contains data all the data that describes the block
-// With comments ommited
+// With comments omitted
 // Content variable represents the raw data
 // while the Data variable represents the key value pairs
-type Block struct{
-	Name string
+type Block struct {
 	Content []byte
-	Data map[string]string
+	Data    map[string]string
 }
 
 // Lines Method is used to return a slice of all lines in the config file
 // if trimmed flag is false comments will be included
-// otherwise the comments are excluded from the result 
-func (c *INI) Lines(trimmed bool) [][]byte{
+// otherwise the comments are excluded from the result
+func (c *INI) Lines(trimmed bool) [][]byte {
 	lines := make([][]byte, 0)
 	var b *bytes.Buffer
-	if !trimmed{
+	if !trimmed {
 		b = bytes.NewBuffer(c.Raw)
 	} else {
 		b = bytes.NewBuffer(c.RawTrimmed)
 	}
 	loop := true
-	for loop{
+	for loop {
 		line, err := b.ReadBytes(LineEnd)
 		loop = err != io.EOF
 		if err != nil && err != io.EOF {
-				log.Fatal("eror reading a line:", err)
+			log.Fatal("error reading a line:", err)
 		}
-		if len(line) == 0{
+		if len(line) == 0 {
 			continue
 		} else {
 			lines = append(lines, line)
 		}
 	}
-
 	return lines
 }
-
 
 // Returns a pointer to unparsed INI struct
 // Raw attribute should contain all the file contents as a byte slice
@@ -77,16 +76,20 @@ func NewINI(pathToIni string) *INI {
 	}
 	defer f.Close()
 	b, err := io.ReadAll(f)
-	
+	for _, val := range os.Environ() {
+		if isWindows = (val == "OS=Windows_NT"); isWindows {
+			b = bytes.ReplaceAll(b, []byte(WindowsLineEnd), []byte{LineEnd})
+			break
+		}
+	}
 	if err != nil {
 		log.Fatal("Failed to open INI file:", err)
 	}
 	return &INI{
-		Raw: b,
-		Blocks: make([]Block, 0),
+		Raw:    b,
+		Blocks: make(map[string]Block),
 	}
 }
-
 
 // Parses all the data in the INI file which includes
 // Removing comments
@@ -98,67 +101,65 @@ func (c *INI) Parse() {
 	extractBlocks(c)
 }
 
-
-func extractBlocks(c *INI){
+func extractBlocks(c *INI) {
 	lines := c.Lines(true)
 	regexpCh := regexp.MustCompile(`\[([\w\$-]+)\]`)
-	var blk Block
+	var (
+		blk  Block
+		name string
+	)
 	cnt := 0
-	for i, line := range lines {
-		if regexpCh.Match(line){
+	for _, line := range lines {
+		if regexpCh.Match(line) {
 			if cnt != 0 {
-				c.Blocks = append(c.Blocks, blk)
+				name = regexpCh.FindStringSubmatch(string(line))[1]
+				c.Blocks[name] = blk
 			}
 			blk = Block{
-				Name: regexpCh.FindStringSubmatch(string(line))[1],
 				Content: make([]byte, 0),
-				Data: make(map[string]string),
+				Data:    make(map[string]string),
 			}
 			blk.AppendContent(line)
 			cnt++
 		} else if cnt != 0 {
-			kv := strings.Split(string(line), KeyValSep)
-			if validKey(kv[0]) && len(kv) == 2{
+			key, value, found := strings.Cut(string(line), KeyValSep)
+			if found && validKey(key) {
 				blk.AppendContent(line)
-				
+				kv := []string{key, value}
 				blk.AddData(kv)
-			} else if len(kv) == 2 {
+			} else if found {
 				log.Fatal("A key can not contain the ';' character in its name")
-			} else if len(kv) <2 {
-				log.Panicf("Line %d is invalid, is not a valid key value pair of a comment", i)
 			}
 		}
 	}
-	c.Blocks = append(c.Blocks, blk)
-	
+	c.Blocks[name] = blk
 }
 
-
-func validKey(key string) bool{
+func validKey(key string) bool {
 	return !strings.Contains(key, ";")
 }
 
-func (b *Block) AppendContent(c []byte){
+func (b *Block) AppendContent(c []byte) {
 	b.Content = append(b.Content, c...)
 }
 
-func (b *Block) AddData(kv []string){
+func (b *Block) AddData(kv []string) {
 	b.Data[kv[0]] = kv[1]
 }
 
 // Trims out all lines starting with ;
-// and all content starting with ; preceeded by a blank character
-func trimComments(c *INI){
+// and all content starting with ; preceded by a blank character
+func trimComments(c *INI) {
 	lines := c.Lines(false)
 	res := make([]byte, 0)
 	for _, line := range lines {
-		
+
 		if line[0] == CommentStart {
 			continue
 		} else {
-			for i, x := range line{
-				if x == CommentStart && (line[i-1] == ' ' || line[i-1] == '\t' || line[i-1] == ']'){
-					line = line[0:i]
+			for i, x := range line {
+				if x == CommentStart && (line[i-1] == ' ' || line[i-1] == '\t' || line[i-1] == ']') {
+					line = line[0 : i-1]
 					line = append(line, '\n')
 					break
 				}
---
+++ iniparser_test.go
@@ -0,0 +1,72 @@
+package iniparser_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/skowe/iniparser"
+)
+
+var expectedRaw = []byte(`[Block1]
+;A comment line
+key1=val1
+key2=val2 ;A comment at the end of line
+key3=valwith; a comment
+key3=val with more words
+
+[Block2]
+;this = should't load
+key1b2=123`)
+
+var expectedParsed = `[Block1]
+key1=val1
+key2=val2
+key3=valwith; a comment
+key3=val with more words
+
+[Block2]
+key1b2=123`
+
+var testini = iniparser.NewINI("./default.ini")
+
+// Conversion to string is done in case of testing on windows
+
+func TestRaw(t *testing.T) {
+
+	if !bytes.Equal(testini.Raw, expectedRaw) {
+		var badChPos int
+		var badCh, expectedCh byte
+		for i, val := range testini.Raw {
+			if val != expectedRaw[i] {
+				badCh = val
+				badChPos = i
+				expectedCh = expectedRaw[i]
+				break
+			}
+		}
+		t.Errorf("Raw data from file does not match expected values, got %d expected %d on position %d", badCh, expectedCh, badChPos)
+
+	}
+}
+
+func TestParser(t *testing.T) {
+	testini.Parse()
+	s1 := string(testini.RawTrimmed)
+
+	if !(s1 == expectedParsed) {
+		if len(s1) == len(expectedParsed) {
+			var badChPos int
+			var badCh, expectedCh rune
+			for i, val := range s1 {
+				if val != rune(expectedParsed[i]) {
+					badCh = val
+					badChPos = i
+					expectedCh = rune(expectedParsed[i])
+				}
+			}
+			t.Errorf("Raw data from file does not match expected values, got %d expected %d on position %d", badCh, expectedCh, badChPos)
+		} else {
+			t.Errorf("Expected data and raw data are not the same length got %d, expected %d", len(testini.RawTrimmed), len(expectedParsed))
+		}
+	}
+}
